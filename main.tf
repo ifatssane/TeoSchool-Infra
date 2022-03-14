b2dc2520a23bfcbd82dcd284e5e874eea4a0d3c2
jaouad.ifatssane <jaouad.ifatssane@gmail.com>
2022-03-14 15:37:24 +0100

    first test commit

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
-
+jaou
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
