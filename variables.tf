79fa251bbbc539fc9a745a8fb75fbdd0ed776d48
jaouad.ifatssane <jaouad.ifatssane@gmail.com>
2022-03-15 11:31:31 +0100

    add K8S conf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,24 @@
-variable "region" {
-  description = "AWS region"
-  default     = "us-west-1"
+variable "resource_group_name" {
+  type        = string
+  description = "RG name in Azure"
 }
-
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+variable "location" {
+  type        = string
+  description = "Resources location in Azure"
 }
-
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+variable "cluster_name" {
+  type        = string
+  description = "AKS name in Azure"
 }
-
+variable "kubernetes_version" {
+  type        = string
+  description = "Kubernetes version"
+}
+variable "system_node_count" {
+  type        = number
+  description = "Number of AKS worker nodes"
+}
+variable "acr_name" {
+  type        = string
+  description = "ACR name"
+}
